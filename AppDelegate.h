7bc53f4e6151a30188927ca7122c12113e521cd7
jifeng <iosjfeng@163.com>
2019-09-05 10:56:43 +0800

    first commit

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCDemo
+//
+//  Created by Ji Feng on 2019/2/23.
+//  Copyright © 2019 Ji Feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
